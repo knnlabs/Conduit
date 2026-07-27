06b31c1737b2d62d4901ac9ba3f9a9269b77590d
Nick Nassiri <nick206@gmail.com>
2026-07-26 23:02:31 -0700

    refactor(webadmin): remove orphaned cost dashboard
    Closes #1331

--- WebAdmin/src/app/cost-dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const CostDashboard = dynamic(() => import('./CostDashboard'), {
-  loading: () => <div>Loading cost dashboard...</div>,
-});
-
-export default CostDashboard;
